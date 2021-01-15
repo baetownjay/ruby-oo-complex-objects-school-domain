06bf0ac3a97dcf8a03223eab8919581433f1a981
Jason Herrera <baytownjason@yahoo.com>
2021-01-14 22:23:29 -0600

    commit tree bb7be54301b90fce673c2945f8aedc611459c2b4

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,38 @@
 # code here!
+require 'pry'
+
+class School
+    attr_accessor :add_student, :roster
+    attr_reader :student, :grade
+    @@all = []
+
+    def initialize(school)
+        @school = school
+        @roster = roster = {}
+    end
+    
+
+    def add_student(student, grade)
+        @roster.has_key?(grade) ? @roster[grade] << student : @roster[grade] = student.split("  ")
+        @@all << @roster
+        #binding.pry
+    end
+
+    def self.all
+        @@all
+    end
+
+    def grade(number)
+        number = [number]
+        targeted_students = []
+        @@all.select do |target_num|
+            #binding.pry
+            if target_num == number
+                targeted_students << target_num
+                #binding.pry
+            end
+        end
+        targeted_students
+    end
+
+end
